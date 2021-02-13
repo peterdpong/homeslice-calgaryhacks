84f3e108c713dfa7d95f4738319c2b3e21ec3cc5
Soraphoj(Peter) Damrongpiriyapong <peterdpong@gmail.com>
2021-02-13 17:21:41 -0500

    Added react router implementation

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,16 +1,52 @@
 import './App.css';
 import NavBar from "./components/navbar/NavBar"
-import { Container } from '@chakra-ui/react'
+import { Container, Heading, Text, Button, Box } from '@chakra-ui/react'
+import { Switch, Route, BrowserRouter as Router, withRouter } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <NavBar/>
-      <Container>
-        Welcome to HomeSlice.
-      </Container>
+    <Router>
+      <div className="App">
+        <NavBar/>
+        <Switch>
+          <Route exact path="/">
+            <Container centerContent>
+              <Heading fontSize="5xl">Welcome to HomeSlice.</Heading>
+              <Text fontSize="lg">Visualizing housing data to tackle housing problems in Canada. </Text>
+              <Button size="lg" m="5">Explore cities</Button>
+            </Container>
 
-    </div>
+            <Box m="5">
+                <Heading fontSize="3xl" marginBottom="2">About the problem</Heading>
+                <Box borderRadius="lg" bgColor="gray.100" padding="5">
+                  <Text fontSize="lg">INFO ABOUT Problem</Text>
+                </Box>
+            </Box>
+
+            <Box m="5">
+                <Heading fontSize="3xl" marginBottom="2">HomeSlice's goal</Heading>
+                <Box borderRadius="lg" bgColor="gray.100" padding="5">
+                  <Text fontSize="lg">INFO ABOUT Solution</Text>
+                </Box>
+            </Box>
+          </Route>
+          
+          <Route path="/toronto">
+
+          </Route>
+          <Route path="/about">
+
+          </Route>
+          <Route path="/explore">
+            <Text fontSize="lg">INFO ABOUT Solution</Text>
+          </Route>
+          
+          
+
+        </Switch>
+
+      </div>
+    </Router>
   );
 }
 
