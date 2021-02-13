c696771c8bea6941540d69fae6836f52bc282dd1
Soraphoj(Peter) Damrongpiriyapong <peterdpong@gmail.com>
2021-02-13 15:20:30 -0500

    Add Chakra UI and react router

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ChakraProvider>
+    <BrowserRouter>
+      <App /> 
+    </BrowserRouter>
+  </ChakraProvider>,
   document.getElementById('root')
 );
 
