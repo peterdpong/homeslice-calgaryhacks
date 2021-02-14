59dac5031c690287ec7f1e3860d9f9cd10e3b8fe
Ethan Zheng <ethanzheng11@gmail.com>
2021-02-13 20:06:31 -0500

    Added functionality to the website.

--- front-end/src/components/navbar/Logo.js
+++ front-end/src/components/navbar/Logo.js
@@ -1,13 +1,16 @@
 import React from "react";
-import { Box, Text } from '@chakra-ui/react';
+import { Box, Text, Link, Button } from '@chakra-ui/react';
 import { Image } from "@chakra-ui/react";
+import {Link as ReactLink} from 'react-router-dom'
 
 export default function Logo(props) {
   return (
-    <Box {...props}>
+    <Box {...props} as = "Button"> 
+    <Link as = {ReactLink} to="/">
       <Image
       src = './HomeSlice.png'
       />
-    </Box>
+      </Link>
+    </Box> 
   )
 }
