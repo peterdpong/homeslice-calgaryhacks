18e6f07f53c6e4ba141c764ae403c5d02e80b98d
Ethan Zheng <ethanzheng11@gmail.com>
2021-02-14 06:35:48 -0500

    Edited the Calgary page.

--- src/components/pages/Calgary.js
+++ src/components/pages/Calgary.js
@@ -1,17 +1,16 @@
 import React from 'react'
-import { Container, Heading, Text, Image } from '@chakra-ui/react'
+import { Container, Heading, Text, Image, Center } from '@chakra-ui/react'
 
 const CalgaryPage = () => {
   return (
     <div>
-      <Container marginBottom="10" centerContent>
+      <Center>
         <Image
               w = '500px' mx = 'auto'
               src = './calgarySil.png'
-              />
-        <Heading margin="5" fontSize="4xl">Calgary Housing Overview</Heading>
-        <Text fontSize="xl">Coming soon when City of Calgary has data ;)</Text>
-      </Container>
+              /></Center>
+        <Center><Heading margin="5" fontSize="4xl">Calgary Housing Overview</Heading></Center>
+        <Center><Text fontSize="xl">Coming soon when the City of Calgary has data ;)</Text></Center>
       
     </div>
   )
