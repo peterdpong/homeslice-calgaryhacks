fb122613255f3e64fdc4b69f8a7e0e43163586ac
Ethan Zheng <ethanzheng11@gmail.com>
2021-02-13 22:18:36 -0500

    Added favicon and descriptions of HomeSlice.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -12,7 +12,7 @@ function App() {
         <Switch>
           <Route exact path="/">
             <Container centerContent>
-              <Heading fontSize="4xl">Welcome to HomeSlice.</Heading>
+              <Heading fontSize="4xl">Welcome to HomeSlice</Heading>
               <Text fontSize="md">Visualizing housing data to tackle housing problems in Canada. </Text>
               <Link to="/explore"> <Button colorScheme = "blue" size="lg" m="5">Explore cities</Button> </Link>
             </Container>
@@ -25,14 +25,19 @@ function App() {
             <Box m="5">
                 <Heading fontSize="3xl" marginBottom="2">About The Problem</Heading>
                 <Box borderRadius="lg" bgColor="gray.100" padding="5">
-                  <Text fontSize="lg">INFO ABOUT Problem</Text>
+                  <Text fontSize="lg"> In major Canadian cities, there exists a rising issue of poverty and homelessness.
+                  Even though these are increasing problems within our society, there is a severe lack of social housing to help counter
+                  these difficulties that many Canadians face.
+                  </Text>
                 </Box>
             </Box>
 
             <Box m="5">
                 <Heading fontSize="3xl" marginBottom="2">HomeSlice's Goal</Heading>
                 <Box borderRadius="lg" bgColor="gray.100" padding="5">
-                  <Text fontSize="lg">INFO ABOUT Solution</Text>
+                  <Text fontSize="lg">The goal of HomeSlice is to allow for the visualization of data that deals with the
+                  growing rates of homelessness that plague Canadian communities. Our hope is that this will influence municipal governments
+                   to incorporate more social housing units in neighbourhoods that desperately need them. </Text>
                 </Box>
             </Box>
           </Route>
